9cea9932da34ad4425f807ea93b152f0dfc97257
petermusembi69 <nyongesamusembi@gmail.com>
2021-05-15 15:07:06 +0300

    Set up files and localization

---
+++ day5_meditation_app/lib/ui/play_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlayListPage extends StatefulWidget {
+  PlayListPage({Key? key}) : super(key: key);
+
+  @override
+  _PlayListPageState createState() => _PlayListPageState();
+}
+
+class _PlayListPageState extends State<PlayListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('');
+  }
+}
---
+++ day5_meditation_app/lib/utils/_index.dart
@@ -0,0 +1,5 @@
+library day5_utils;
+
+part 'audio_card.dart';
+part 'play_list_card.dart';
+part 'top_creators_card.dart';
