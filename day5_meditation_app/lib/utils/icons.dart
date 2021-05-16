142ef321e352776885078651a708791449c498da
petermusembi69 <nyongesamusembi@gmail.com>
2021-05-16 15:06:28 +0300

    Add custom icon

---
+++ day5_meditation_app/lib/utils/icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData film_play =
+      IconData(0xe824, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
