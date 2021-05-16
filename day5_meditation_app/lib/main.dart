af807efdf86c0ba05f91431a77f1f75f61b16c05
petermusembi69 <nyongesamusembi@gmail.com>
2021-05-16 11:30:04 +0300

    Complete login screen

--- day5_meditation_app/lib/main.dart
+++ day5_meditation_app/lib/main.dart
@@ -14,10 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Poppins'
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Poppins'),
       localizationsDelegates: [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
--- day5_meditation_app/lib/ui/login_page.dart
+++ day5_meditation_app/lib/ui/login_page.dart
@@ -44,22 +44,25 @@ class _LoginPageState extends State<LoginPage> {
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.only(top:198.0),
+            child: FractionallySizedBox(
+              heightFactor: 0.75,
+              alignment: Alignment.bottomCenter,
+              child: SingleChildScrollView(
                 child: Container(
-                  height: double.maxFinite,
-                  width: MediaQuery.of(context).size.width * 0.8,
+                  width: MediaQuery.of(context).size.width * 0.85,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        l10n.meditateTitle,
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w800,
-                          fontSize: 60,
-                          color: Colors.white,
+                      Container(
+                        height: 60,
+                        child: Text(
+                          l10n.meditateTitle,
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w800,
+                            fontSize: 55,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                       Text(
@@ -67,8 +70,8 @@ class _LoginPageState extends State<LoginPage> {
                         style: TextStyle(
                           fontFamily: 'Poppins',
                           fontWeight: FontWeight.w300,
-                          fontSize: 35,
-                          color: Colors.white,
+                          fontSize: 33,
+                          color: Colors.white.withOpacity(0.9),
                         ),
                       ),
                       Text(
@@ -77,74 +80,92 @@ class _LoginPageState extends State<LoginPage> {
                           fontFamily: 'Poppins',
                           fontWeight: FontWeight.w300,
                           fontSize: 15,
-                          color: Colors.white,
+                          color: Colors.white.withOpacity(0.8),
                         ),
                       ),
-                      Container(
-                        padding: EdgeInsets.only(
-                            left: 20, right: 20, top: 30, bottom: 10),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.4,
+                          padding: EdgeInsets.only(
+                              left: 30, right: 30, top: 30, bottom: 10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
                           ),
-                        ),
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              decoration: InputDecoration(
-                                suffixIcon: Icon(Icons.mail),
-                                hintText: l10n.email,
-                                fillColor: Colors.blue.withOpacity(0.5),
-                                focusColor: Colors.blue.withOpacity(0.5),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  suffixIcon: Icon(Icons.mail),
+                                  hintText: l10n.email,
+                                  fillColor: Colors.black
+                                      .withBlue(238)
+                                      .withGreen(220)
+                                      .withRed(213),
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 15),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(10),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              decoration: InputDecoration(
-                                suffixIcon: Icon(Icons.lock),
-                                hintText: l10n.password,
-                                fillColor: Colors.blue.withOpacity(0.5),
-                                focusColor: Colors.blue.withOpacity(0.5),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(10),
+                              TextFormField(
+                                keyboardType: TextInputType.visiblePassword,
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  suffixIcon: Icon(Icons.lock),
+                                  hintText: l10n.password,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 15),
+                                  fillColor: Colors.black
+                                      .withBlue(238)
+                                      .withGreen(220)
+                                      .withRed(213),
+                                  border: OutlineInputBorder(
+                                    borderSide: BorderSide.none,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(10),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            MaterialButton(
-                              onPressed: () {},
-                              color: Colors.orange,
-                              minWidth: double.maxFinite,
-                              child: Text(
-                                l10n.login,
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white,
+                              MaterialButton(
+                                onPressed: () {},
+                                color: Colors.orange,
+                                minWidth: double.maxFinite,
+                                child: Text(
+                                  l10n.login,
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Text(l10n.doNotHaveAccount),
-                            Text(
-                              l10n.createAccount,
-                              style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w600,
+                              Column(
+                                children: [
+                                  Text(l10n.doNotHaveAccount),
+                                  Text(
+                                    l10n.createAccount,
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ],
